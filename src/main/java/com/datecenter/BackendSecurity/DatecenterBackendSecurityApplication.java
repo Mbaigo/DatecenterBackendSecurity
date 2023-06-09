8e437ebc2b47eb571498d157c7dab60ebea57ffd
Mbaigo <mbaigolmemeevrard@gmail.com>
2023-06-09 08:13:05 +0200

    Premier commit

---
+++ src/main/java/com/datecenter/BackendSecurity/DatecenterBackendSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.datecenter.BackendSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatecenterBackendSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatecenterBackendSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datecenter/BackendSecurity/DatecenterBackendSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datecenter.BackendSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatecenterBackendSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
